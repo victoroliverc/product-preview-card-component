ed6461c5ba40d883484b6089f7de70239f55f4e9
Victor Cunha <victor.oliverc@outlook.com>
2024-01-18 21:35:17 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Fraunces", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
